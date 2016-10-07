cef6956191f82f4ab1ad7319ae134901bc5e2b6b
Expiravit <avalongames@outlook.com>
2016-10-07 17:32:57 +0300

    Добавил тест

--- TextAnalyzer/src/TestingCode.java
+++ TextAnalyzer/src/TestingCode.java
@@ -5,56 +5,48 @@ public class TestingCode {
     private TextAnalyzer textAnalyzer;
 
     @Test
-    public void getWordsTest(){
+    public void getWordsTest() {
         textAnalyzer = new TextAnalyzer("Test test text test");
         String[] actual = {"test", "test", "text", "test"};
-        String [] result = new String[textAnalyzer.getWords().size()];
+        String[] result = new String[textAnalyzer.getWords().size()];
         result = textAnalyzer.getWords().toArray(result);
         Assert.assertArrayEquals(result, actual);
     }
 
     @Test
-    public void getWordsTest2(){
-        textAnalyzer = new TextAnalyzer("Test test text test");
-        String[] actual = {"test", "test", "text", "test"};
-        String [] result = new String[textAnalyzer.getWords().size()];
+    public void getWordsTest2() {
+        textAnalyzer = new TextAnalyzer("");
+        String[] actual = {};
+        String[] result = new String[textAnalyzer.getWords().size()];
         result = textAnalyzer.getWords().toArray(result);
         Assert.assertArrayEquals(result, actual);
     }
 
     @Test
-    public void getMostFrequentWordTest(){
+    public void getMostFrequentWordTest() {
         textAnalyzer = new TextAnalyzer("Test test text test");
         String result = textAnalyzer.getMostFrequentWord();
         String actual = "test";
-        Assert.assertEquals(result , actual);
+        Assert.assertEquals(result, actual);
     }
 
     @Test
-    public void getMostFrequentWordTest2(){
+    public void getMostFrequentWordTest2() {
         textAnalyzer = new TextAnalyzer("Test text test");
         String result = textAnalyzer.getMostFrequentWord();
         String actual = "test";
-        Assert.assertEquals(result , actual);
-    }
-
-    @Test
-    public void getMostFrequentWordTest3(){
-        textAnalyzer = new TextAnalyzer("text test");
-        String result = textAnalyzer.getMostFrequentWord();
-        String actual = "test";
-        Assert.assertEquals(result , actual);
+        Assert.assertEquals(result, actual);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void getMostFrequentWordTest4(){
+    public void getMostFrequentWordTest3() {
         textAnalyzer = new TextAnalyzer("");
         String result = textAnalyzer.getMostFrequentWord();
-        Assert.assertNull(result );
+        Assert.assertNull(result);
     }
 
-    @Test
-    public void getMostFrequentWordTest5(){
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getMostFrequentWordTest4() {
         textAnalyzer = new TextAnalyzer("тест текст на русском тест");
         String result = textAnalyzer.getMostFrequentWord();
         String actual = "тест";
